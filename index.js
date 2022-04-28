3e2fa406781d6389e97e76ba55a5cde260e277d4
DESKTOP-9O4290A\Mike <faulkner.johnfogerty@gmail.com>
2022-04-27 22:03:27 -0400

    Added Compounding function

--- index.js
+++ index.js
@@ -4,10 +4,14 @@ let dollarAmount = document.getElementById("dollarAmount");
 let interestRate = document.getElementById("interestRate");
 let calculate = document.getElementById("calculate");
 let output = document.getElementById("output");
+let time = document.getElementById("time");
 
-function calculateInterest(money, interest) {
-  let result = money * interest + money;
-  return result;
+function calculateInterest(money, interest, time) {
+  for (x = 0; x < time; x++) {
+    console.log(x, money, interest, time);
+    money += money * interest;
+  }
+  return money;
 }
 
 // calculate.addEventListener("click", calculateInterest)
@@ -17,6 +21,7 @@ calculate.addEventListener("click", function (e) {
   console.log(interestRate.value);
   let money = Number(dollarAmount.value);
   let interest = Number(interestRate.value);
-  let result = calculateInterest(money, interest);
+  let years = Number(time.value);
+  let result = calculateInterest(money, interest, years);
   output.innerHTML = result;
 });
